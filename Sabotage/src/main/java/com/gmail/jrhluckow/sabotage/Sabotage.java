8226fb24e3eae1a881e0f427a68aaf9c6df0d621
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 14:47:27 -0300

    Initial Commit

---
+++ Sabotage/src/main/java/com/gmail/jrhluckow/sabotage/Sabotage.java
@@ -0,0 +1,17 @@
+package com.gmail.jrhluckow.sabotage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sabotage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
